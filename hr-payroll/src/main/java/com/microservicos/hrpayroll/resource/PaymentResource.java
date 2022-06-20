d176dde3ee2efcc0486b46f40473a85851e1dd91
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-06-20 16:33:45 -0300

    Adicionando Hystrix

--- hr-payroll/src/main/java/com/microservicos/hrpayroll/resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/microservicos/hrpayroll/resource/PaymentResource.java
@@ -4,6 +4,7 @@ import com.microservicos.hrpayroll.client.WorkerClient;
 import com.microservicos.hrpayroll.client.dto.WorkerDTO;
 import com.microservicos.hrpayroll.domain.entity.Payment;
 import com.microservicos.hrpayroll.domain.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +22,14 @@ public class PaymentResource {
     @Autowired
     private WorkerClient workerClient;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable("workerId") Long workerId, @PathVariable("days") Integer days){
         WorkerDTO workerDTO = workerClient.findById(workerId).getBody();
         return ResponseEntity.ok(paymentService.getPayment(workerDTO.getName(),workerDTO.getDailyIncome(),days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative( Long workerId, Integer days){
+        return ResponseEntity.ok( new Payment("Brann",400.00,days));
     }
 }
