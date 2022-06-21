90a706838bc51bd08f474c7ceb7459de86fd4c5d
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-06-21 13:20:33 -0300

    Ajuste Actuator

--- hr-worker/src/main/java/com/microservicos/hrworker/resource/WorkerResource.java
+++ hr-worker/src/main/java/com/microservicos/hrworker/resource/WorkerResource.java
@@ -5,6 +5,7 @@ import com.microservicos.hrworker.dto.WorkerDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @Slf4j
 @RestController
 @RequestMapping("/workers")
